561988d581b4ee19d3911b7d4c8aa8a5fd8d9a9f
Yustinia <yustinia@protonmail.com>
2026-01-21 11:37:22 +0800

    finalized lockscreen look

---
+++ .config/niri/scripts/hyprlockScreen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WALL_DIR="$HOME/Pictures/Walls"
+
+rm -f "$WALL_DIR/lockscreen.png"
+grim "$WALL_DIR/lockscreen.png"
+# grim - | magick - \
+#   -resize 960x540! \
+#   # -blur 0x6 \
+#   # -brightness-contrast -5x10 \
+#   # -modulate 95,80,100 \
+#   "$WALL_DIR/lockscreen.png"
+hyprlock -c "$HOME/.config/niri/modules/hypr/hyprlock.conf"
+
+exit 0
