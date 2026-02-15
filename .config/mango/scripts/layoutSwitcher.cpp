fdd5c219d890e0f5f75a7733e2443ab5516f3e43
Yustinia <yustinia@protonmail.com>
2026-02-15 23:58:04 +0800

    add layout switching

---
+++ .config/mango/scripts/layoutSwitcher.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <format>
+
+int main()
+{
+    std::vector<std::string> states = {
+        "T",
+        "S",
+        "G",
+        "CT",
+        "VS",
+        "VT",
+        "VG",
+        "RT",
+    };
+
+    std::string currentState;
+    std::cin >> currentState;
+
+    auto currentIndex = std::find(states.begin(), states.end(), currentState);
+
+    if (currentIndex != states.end())
+    {
+        size_t index = std::distance(states.begin(), currentIndex);
+
+        size_t nextIndex = (index + 1) % states.size();
+
+        std::cout << states[nextIndex];
+    }
+
+    return 0;
+}
